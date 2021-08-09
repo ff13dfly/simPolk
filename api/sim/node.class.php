a27eb4570a93e617e5ffa4fe5f40ed20ef58c61a
fuu <fu@yihaofang.com>
2021-08-09 15:55:05 +0800

    mining simulate works well.

--- api/sim/node.class.php
+++ api/sim/node.class.php
@@ -9,8 +9,13 @@ class Node{
 		$this->cur=$cur;
 		$this->db=$core;
 		
+		$result=array(
+			'success'=>false,
+		);
+
 		switch ($act) {
 			case 'list':
+				
 				return array(
 					'success'	=>	TRUE,
 					'node'		=>	$cfg['nodes'],
@@ -24,10 +29,44 @@ class Node{
 				return $this->viewSample($param);
 				break;
 
+			case 'mining':
+				//echo 'hello world';
+				$res=$this->miningSimulator($param);
+				if($res!=false){
+					$result['success']=true;
+					$result['code']=$res;
+				}
+				break;
+
 			default:
 				
 				break;
 		}
+		return $result;
+	}
+
+	private function miningSimulator($param){
+		$pre=$param['pre'];
+		$words=$param['s'];
+		$check=$param['di'];
+		//strlen($param['di']);
+		
+		$str=$pre.'_'.$words;
+		$step=$param['step'];
+		$n=$param['n'];
+
+		$start=$n*($step-1);
+		for($i=$start;$i<$n+$start;$i++){
+			$hash='0x'.hash('sha256',$str.'_'.$i);
+			if(substr($hash, 0,strlen($check))===$check){
+				return array(
+					'string'	=>	$str.'_'.$i,
+					'hash'		=>	$hash,
+					'type'		=>	'sha256'
+				);
+			}
+		}
+		return false;
 	}
 
 	private function viewSample($param){
